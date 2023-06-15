8d3cb61d7a2cb2dc3bee48e0c0d4911e0bf057bc
Sineway <sineway@yandex.com>
2023-06-15 09:23:02 +0300

    Добавим описание объектов, которые планируем генерировать

---
+++ js/types.js
@@ -0,0 +1,16 @@
+/**
+ * @typedef Picture
+ * @prop {number} id
+ * @prop {string} url
+ * @prop {string} description
+ * @prop {number} likes
+ * @prop {Array<PictureComment>} comments
+ */
+
+/**
+ * @typedef PictureComment
+ * @prop {number} id
+ * @prop {string} avatar
+ * @prop {string} message
+ * @prop {string} name
+ */
