99668366af656b91445da08e5ca6e5bcd0f2f83a
Sineway <sineway@yandex.com>
2023-06-15 13:47:23 +0300

    Добавим генерацию комментариев

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@ const descriptions = [
   'Описание 3',
 ];
 
+const messages = [
+  'Комментарий 1',
+  'Комментарий 2',
+  'Комментарий 3',
+];
+
+const names = [
+  'Имя 1',
+  'Имя 2',
+  'Имя 3',
+];
+
 /**
  * @param {number} length
  * @returns {Array<Picture>}
@@ -22,10 +34,31 @@ function createPicture(id) {
   const url = `photos/${id}.jpg`;
   const description = pickItemFromArray(descriptions);
   const likes = pickIntegerInRange(15, 200);
-  // TODO: createPictureCommentArray
-  const comments = [];
+  const comments = createPictureCommentArray(pickIntegerInRange(0, 30));
 
   return {id, url, description, likes, comments};
+}
+
+/**
+ * @param {number} length
+ * @returns {Array<PictureComment>}
+ */
+function createPictureCommentArray(length) {
+  const items = new Array(length).fill(1);
+
+  return items.map((start, index) => createPictureComment(start + index));
+}
+
+/**
+ * @param {number} id
+ * @returns {PictureComment}
+ */
+function createPictureComment(id) {
+  const avatar = `img/avatar-${pickIntegerInRange(1, 6)}.svg`;
+  const message = pickItemFromArray(messages);
+  const name = pickItemFromArray(names);
+
+  return {id, avatar, message, name};
 }
 
 /**
