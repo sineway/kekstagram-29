a4fd2b2daa84ef5a55049c9be0ca0fdf70a97d3c
Sineway <sineway@yandex.com>
2023-06-19 11:31:40 +0300

    Добавим функцию для проверки длительности встречи

--- js/utils.js
+++ js/utils.js
@@ -55,10 +55,44 @@ function parseDigits(value) {
   return digits ? Number(digits) : NaN;
 }
 
+/**
+ * Поверит не выходит ли встреча за рамки рабочего дня.
+ * @param {string} workStart - Начало рабочего дня в формате `часы:минуты`.
+ * @param {string} workEnd - Конец рабочего дня в формате `часы:минуты`.
+ * @param {string} meetingStart - Начало встречи в формате `часы:минуты`.
+ * @param {number} meetingDuration - Продолжительность встречи в минутах.
+ * @returns {boolean}
+ */
+function isWithinWorkingDay(workStart, workEnd, meetingStart, meetingDuration) {
+  const workStartTime = parseTime(workStart);
+  const workEndTime = parseTime(workEnd);
+  const meetingStartTime = parseTime(meetingStart);
+
+  return (
+    meetingStartTime >= workStartTime &&
+    meetingDuration <= workEndTime - meetingStartTime
+  );
+}
+
+/**
+ * Извлечёт количество минут из строки вида `часы:минуты`.
+ * @param {string} time
+ * @returns {number}
+ */
+function parseTime(time) {
+  const parts = time.split(':').map(Number);
+  const [hours, minutes] = parts;
+  const minutesPerHour = 60;
+
+  return hours * minutesPerHour + minutes;
+}
+
 export {
   pickItemFromArray,
   pickIntegerInRange,
   fitsLength,
   isPalindrome,
   parseDigits,
+  isWithinWorkingDay,
+  parseTime,
 };
