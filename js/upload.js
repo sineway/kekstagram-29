1e8ca78105d292bd66372759c340aa33bc4520f9
Sineway <sineway@yandex.com>
2023-07-07 15:50:29 +0300

    Добавим проверку хэш-тегов и описания с помощью библиотеки Pristine

--- js/upload.js
+++ js/upload.js
@@ -1,12 +1,20 @@
 import renderPopup from './upload-popup.js';
+import './pristine-validators.js';
 
 /**
  * @type {HTMLFormElement}
  */
 const form = document.querySelector('.img-upload__form');
 
+// @ts-ignore
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+});
+
 form.addEventListener('change', onFormChange);
 form.addEventListener('hide', onFormHide, true);
+form.addEventListener('reset', onFormReset);
 
 /**
  * @param {Event & {target: HTMLInputElement}} event
@@ -23,3 +31,6 @@ function onFormHide() {
   form.reset();
 }
 
+function onFormReset() {
+  pristine.reset();
+}
