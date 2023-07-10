761acf60adf26254aba5727ed76b86c52d6e3f5c
Sineway <sineway@yandex.com>
2023-07-10 12:54:36 +0300

    Добавим сообщение об ошибке, если не удалось получить данные с сервера

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 import {request} from './utils.js';
 import initGallery from './gallery.js';
+import renderStatus from './status.js';
 import './upload.js';
 
-/**
- * @type {Array<Picture>}
- */
-const data = await request('https://28.javascript.pages.academy/kekstagram/data');
+try {
+  /**
+   * @type {Array<Picture>}
+   */
+  const data = await request('https://28.javascript.pages.academy/kekstagram/data');
 
-initGallery(data);
+  initGallery(data);
+
+} catch (error) {
+  const title = `Ошибка: ${error.message}`;
+  const button = 'Закрыть';
+
+  renderStatus('error', {title, button});
+}
